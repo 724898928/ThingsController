57363ebaa468e03e2c669fd78aef2ce8d8fb8652
care.lee <care.lee@provisionusa.com>
2023-01-16 16:14:56 +0800

    Remove the native-lib

--- app/src/main/java/com/lixin/thingscontroller/MainActivity.java
+++ app/src/main/java/com/lixin/thingscontroller/MainActivity.java
@@ -14,16 +14,6 @@ import com.lixin.view.MyOpenGl;
 
 public class MainActivity extends Activity {
 
-    // Used to load the 'native-lib' library on application startup.
-    static {
-        System.loadLibrary("native-lib");
-    }
-
-    // Used to load the 'native-lib' library on application startup.
-    static {
-        System.loadLibrary("native-lib");
-    }
-
     private static String className = "com.li Main2Activity";
     MyOpenGl myOpenGl;
     MySurfaceView mySurfaceView;
@@ -45,12 +35,6 @@ public class MainActivity extends Activity {
         // TextView tv = (TextView) findViewById(R.id.sample_text);
         //tv.setText(stringFromJNI());
     }
-
-    /**
-     * A native method that is implemented by the 'native-lib' native library,
-     * which is packaged with this application.
-     */
-    public native String stringFromJNI();
 
     @Override
     protected void onPause() {
